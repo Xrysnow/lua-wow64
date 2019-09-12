fc1d88218de1a15ae6fa8e9abe9a3a67f9b8e060
Xrysnow <xrysnow@outlook.com>
2019-09-13 00:52:11 +0800

    update proc.lua

--- proc.lua
+++ proc.lua
@@ -16,6 +16,7 @@ local ReturnType = {
     rt_double = 2, -- double value
     rt_struct = 3, -- structure returned by value
 }
+local _exit_task = {}
 
 local function check(ok)
     if ok < 0 then
@@ -27,6 +28,9 @@ function M.start(path)
     path = path or 'dummy_proc.exe'
     if not _proc then
         _proc = ffi.gc(ffi.new('Process[1]'), function(o)
+            for _, task in ipairs(_exit_task) do
+                task()
+            end
             lib.Process_Terminate(o, 0)
             lib.Process_dtor(o)
         end)
@@ -37,7 +41,12 @@ function M.start(path)
         assert(status >= 0)
         lib.Process_Resume(_proc)
         helper.sleep(100)
+        M.LoadLibrary('msvcrt.dll')
     end
+end
+
+function M.atexit(f)
+    table.insert(_exit_task, f)
 end
 
 function M.setMod(v)
@@ -64,9 +73,9 @@ function M.call(name, ...)
         elseif rawType == 'bool' then
             retVal = ffi.new('int32_t[1]')
         elseif rawType == 'string' then
-            retVal = ffi.new('const char*[1]')
+            retVal = ffi.new('uint64_t[1]')
         elseif rawType == 'wstring' then
-            retVal = ffi.new('const wchar_t*[1]')
+            retVal = ffi.new('uint64_t[1]')
         else
             retVal = ffi.new(string.format('%s[1]', rawType))
         end
@@ -88,6 +97,109 @@ function M.call(name, ...)
     check(ok)
     return retVal[0]
 end
+
+function M.caller(modName)
+    return function(fName, ...)
+        M.setMod(modName)
+        return M.call(fName, ...)
+    end
+end
+
+local cstd = {}
+setmetatable(cstd, { __index = function(t, k)
+    cstd = require('cstd')
+    return cstd[k]
+end })
+
+local function pointer_value(p)
+    if ffi.istype('AsmVariant', p) then
+        p = p.value
+    end
+    assert(ffi.istype('uint64_t', p))
+    return p
+end
+
+function M.alloc(size)
+    assert(size > 0, 'wrong param')
+    local p = lib.Process_MemoryAllocate(_proc, size)
+    assert(p ~= 0, 'failed to alloc')
+    return p
+end
+
+function M.free(p)
+    p = pointer_value(p)
+    assert(ffi.istype('uint64_t', p))
+    if p == 0 then
+        return
+    end
+    assert(lib.Process_MemoryFree(_proc, p))
+end
+
+function M.memRead(src, dst, size)
+    assert(size > 0, 'wrong param')
+    src = pointer_value(src)
+    if dst == nil then
+        dst = ffi.new(string.format('char[%d]', size))
+    elseif ffi.istype('AsmVariant', dst) then
+        dst = dst.value
+    end
+    assert(lib.Process_MemoryRead(_proc, src, dst, size))
+    return dst
+end
+
+function M.memWrite(src, dst, size)
+    assert(size > 0, 'wrong param')
+    if ffi.istype('AsmVariant', src) then
+        src = src.value
+    end
+    dst = pointer_value(dst)
+    assert(lib.Process_MemoryWrite(_proc, src, dst, size))
+end
+
+function M.getRemoteString(src)
+    src = pointer_value(src)
+    assert(src ~= 0)
+    local len = cstd.strlen(src)
+    local buf = ffi.new('char[?]', len)
+    local dst = var.array(buf)
+    cstd.memcpy(dst, src, len)
+    return ffi.string(buf)
+end
+
+function M.derefRemotePointer(src)
+    src = pointer_value(src)
+    assert(src ~= 0)
+    local dst = ffi.new('uint64_t[1]')
+    M.memRead(src, dst, 8)
+    return dst[0]
+end
+
+function M.refRemotePointer(src)
+    src = pointer_value(src)
+    local p = M.alloc(8)
+    local buf = ffi.new('uint64_t[1]')
+    buf[0] = src
+    M.memWrite(buf, p, 8)
+    return p
+end
+
+function M.indexRemotePointer(src, index, dst, itemSize)
+    index = ffi.new('uint64_t', index)
+    src = pointer_value(src)
+    if not dst then
+        dst = ffi.new(string.format('char[%d]', itemSize))
+    elseif type(dst) == 'string' then
+        dst = ffi.new(dst .. '[1]')
+        itemSize = itemSize or ffi.sizeof(dst)
+    else
+        assert(type(dst) == 'cdata')
+        itemSize = itemSize or ffi.sizeof(dst)
+    end
+    M.memRead(src + index * itemSize, dst, itemSize)
+    return dst
+end
+
+--
 
 function M.LoadLibrary(path)
     local args, argv, argc = var.pack_va('const wchar_t*', path)
@@ -123,7 +235,7 @@ end
 
 function M.GetCurrentDirectory()
     local ret = ffi.new('uint32_t[1]')
-    local buf, pbuf = var.array('wchar_t', 260)
+    local buf = var.array('wchar_t', 260)
     local mod = L 'kernel32.dll'
     local args, argv, argc = var.pack_va('int32_t', 260, 'wchar_t*', buf)
     local ok = lib.RemoteCall(
@@ -134,7 +246,7 @@ function M.GetCurrentDirectory()
         print('error in GetCurrentDirectory:', M.GetLastError())
         return
     end
-    return helper.fromWideChar(pbuf)
+    return helper.fromWideChar(buf.value)
 end
 
 return M
